eccba709160e2d135053f1cc3698c864d7e2df7a
Felix <fdbatista@gmail.com>
2024-05-16 11:37:11 +0200

    :wrench: Register DB connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,42 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      name: 'shard1',
+      type: 'postgres',
+      host: 'pg-shard-1',
+      port: 5432,
+      username: 'db',
+      password: 'db',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    TypeOrmModule.forRoot({
+      name: 'shard2',
+      type: 'postgres',
+      host: 'pg-shard-2',
+      port: 5432,
+      username: 'db',
+      password: 'db',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    TypeOrmModule.forRoot({
+      name: 'shard3',
+      type: 'postgres',
+      host: 'pg-shard-3',
+      port: 5432,
+      username: 'db',
+      password: 'db',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
